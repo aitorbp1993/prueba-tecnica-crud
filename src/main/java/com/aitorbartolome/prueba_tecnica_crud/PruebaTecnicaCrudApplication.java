09a4704ce5973380ce8f5c41c371dcde5de283ae
Aitor <aitor.tsf@gmail.com>
2025-11-12 21:30:52 +0100

    feat: Initial project structure from Spring Initializr

---
+++ src/main/java/com/aitorbartolome/prueba_tecnica_crud/PruebaTecnicaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.aitorbartolome.prueba_tecnica_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aitorbartolome/prueba_tecnica_crud/PruebaTecnicaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitorbartolome.prueba_tecnica_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
